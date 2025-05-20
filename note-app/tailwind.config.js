275f68d861a00e2b5b978d6ed4b96bcdbcbc4599
Reed Lathrop <reedlathrop@gmail.com>
2025-05-19 23:11:36 -0500

    initial commit

---
+++ note-app/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          600: "#2563eb",
+          700: "#1d4ed8",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
